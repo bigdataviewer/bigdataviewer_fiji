e5eb37c373c38b2c868ca7ba740451e8e72cd18f
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-07-23 17:38:15 -0400

    remove unnecessary cast

--- src/main/java/bdv/ij/BigDataBrowserPlugIn.java
+++ src/main/java/bdv/ij/BigDataBrowserPlugIn.java
@@ -186,7 +186,7 @@ public class BigDataBrowserPlugIn implements PlugIn
 
 			final JLabel label = ( JLabel ) super.getListCellRendererComponent(
 					list, value, index, isSelected, cellHasFocus );
-			label.setIcon( imageMap.get( ( String ) value ) );
+			label.setIcon( imageMap.get( value ) );
 			label.setHorizontalTextPosition( JLabel.RIGHT );
 			label.setFont( font );
 			return label;
